77ef8508fdb7b9af2b2fa030b808cc45746d2b74
Bowler, Ryan <rbowler@lcs-mba2899.lcs.on.ca>
2017-09-30 10:43:17 -0400

    Curved lines ver 1

--- IterationExamples/Iteration Examples.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -30,6 +30,17 @@ let canvas = Canvas(width: 300, height: 300)
 
 // Below this line, try combining a loop and a single statement to generate the goal
 
+for i in stride(from: 0, through: 300, by: 20) {
+    
+    i
+    
+    // Draw each rung
+    canvas.drawLine(fromX: i, fromY: 0, toX: 300, toY: i)
+    
+    canvas
+    
+}
+
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
