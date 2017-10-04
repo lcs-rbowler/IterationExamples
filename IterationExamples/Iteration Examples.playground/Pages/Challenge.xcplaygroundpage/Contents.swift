5165f958c507a32e7c0c898d0d3da638f5d852ae
Bowler, Ryan <rbowler@lcs-mba2899.lcs.on.ca>
2017-10-04 14:36:56 -0400

    4Squares Ver 1.3

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -17,6 +17,7 @@ let canvas = Canvas(width: 600, height: 600)
 // Below this line, try combining a loop and four statements that draw lines to generate the goal
 //=====================================================================\\
 
+//Bottom-Left Square
 for i in stride(from: 0, through: 300, by: 5) {
     canvas.lineColor = Color.init(hue: i, saturation: 100, brightness: 100, alpha: 100)
     canvas.drawLine(fromX: i, fromY: 0, toX: 300, toY: i)
@@ -25,6 +26,7 @@ for i in stride(from: 0, through: 300, by: 5) {
     canvas.drawLine(fromX: 0, fromY: 300-i, toX: i, toY: 0)
 }
 
+//Top-Left Square
 canvas.translate(byX: 0, byY: 300)
 
 for k in stride(from: 0, through: 301, by: 1) {
@@ -34,10 +36,21 @@ for k in stride(from: 0, through: 301, by: 1) {
     canvas.fillColor = Color(hue: k, saturation: 100, brightness: 100, alpha: 100)
 }
 
+//Top-Right Square
 canvas.translate(byX: 300, byY: 0)
 
-for n in stride(from: 0, through: 300, by: 5) {
-    canvas.drawLine(fromX: n, fromY: 0, toX: 300, toY: n, lineWidth: 10)
+for n in stride(from: 0, through: 150, by: 5) {
+    canvas.lineColor = Color.init(hue: n, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawLine(fromX: 150+n, fromY: 5, toX: 300, toY: 5+n, lineWidth: 10)
+    canvas.lineColor = Color.init(hue: 120-n, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawLine(fromX: 300, fromY: 150+n, toX: 300-n, toY: 300, lineWidth: 10)
+    canvas.lineColor = Color.init(hue: n, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawLine(fromX: 150-n, fromY: 300, toX: 5, toY: 300-n, lineWidth: 10)
+    canvas.lineColor = Color.init(hue: 120-n, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawLine(fromX: 5, fromY: 150-n, toX: 5+n, toY: 5, lineWidth: 10)
 }
+
+//Bottom-Right Square
+
 
 PlaygroundPage.current.liveView = canvas.imageView
