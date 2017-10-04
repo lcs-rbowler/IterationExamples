3100c6f63f71cefc9ea2cc8d12b5965636f9254c
Bowler, Ryan <rbowler@lcs-mba2899.lcs.on.ca>
2017-10-04 10:42:12 -0400

    4Square Ver. 1.0

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -12,12 +12,12 @@ import PlaygroundSupport
 //: ## Add your code below
 
 // Create canvas
-let canvas = Canvas(width: 300, height: 300)
+let canvas = Canvas(width: 600, height: 600)
 
 // Below this line, try combining a loop and four statements that draw lines to generate the goal
 //=====================================================================\\
 
-for i in stride(from: 0, through: 300, by: 20) {
+for i in stride(from: 0, through: 300, by: 5) {
     canvas.lineColor = Color.init(hue: i, saturation: 100, brightness: 100, alpha: 100)
     canvas.drawLine(fromX: i, fromY: 0, toX: 300, toY: i)
     canvas.drawLine(fromX: 300, fromY: i, toX: 300-i, toY: 300)
@@ -25,8 +25,12 @@ for i in stride(from: 0, through: 300, by: 20) {
     canvas.drawLine(fromX: 0, fromY: 300-i, toX: i, toY: 0)
 }
 
-/*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
- */
+canvas.translate(byX: 0, byY: 300)
+
+for k in stride(from: 0, through: 301, by: 1) {
+    k
+    canvas.drawShapesWithBorders = false
+    canvas.drawRectangle(bottomLeftX: -1+k, bottomLeftY: 0, width: 1, height: 300)
+    canvas.fillColor = Color(hue: k, saturation: 100, brightness: 100, alpha: 100)
+}
 PlaygroundPage.current.liveView = canvas.imageView
