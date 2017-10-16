edf92817009641126acb1ff80f0f32ded34c7de7
Bowler, Ryan <rbowler@lcs-mba2899.lcs.on.ca>
2017-10-16 09:47:45 -0400

    Rainbow Circles

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -15,4 +15,18 @@ import PlaygroundSupport
 let canvas = Canvas(width: 600, height: 600)
 
 
+canvas.drawShapesWithBorders = false
+
+for x in stride(from: 50, through: 550, by: 100) {
+    for y in stride(from: 550, through: 50, by: -100) {
+        for z in stride(from: 100, through: 20, by: -20) {
+            let randomColor = random(from: 0, toButNotIncluding: 361)
+            let randomBrightness = random(from: 60, toButNotIncluding: 101)
+            canvas.drawEllipse(centreX: x, centreY: y, width: z, height: z)
+            canvas.fillColor = Color.init(hue: randomColor, saturation: randomBrightness, brightness: randomBrightness, alpha: randomBrightness)
+            
+        }
+    }
+}
+
 PlaygroundPage.current.liveView = canvas.imageView
